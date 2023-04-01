e390b9fae0f55c3656e2e85f754132a74a35111f
PavlenkoIGOR <illusiopard@gmail.com>
2023-04-02 00:04:32 +0300

    Добавьте файлы проекта.

---
+++ Calculator.Tests/Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio;
+using NUnit.Compatibility;
+using NUnit.Framework;
+using Calculator;
+
+namespace Calculator.Tests
+{
+    [TestFixture]
+    public class Class1
+    {
+        CalculatorClass calc = new CalculatorClass(20, 10);
+
+        [Test]
+        public void AdditionalMethodMustReturn30()
+        {
+            Assert.AreEqual(30, calc.Additional()); //Are.Equal утверждает, что два значения равны (bool).
+        }
+
+        [Test]
+        public void SubstractionMethodMustReturn10()
+        {
+            Assert.AreEqual(10, calc.Subtraction());
+        }
+        
+        [Test]
+        public void MultiplicationMethodMustReturn200()
+        {
+            Assert.AreEqual(200, calc.Miltiplication());
+        }
+        [Test]
+        public void DevisionMethodMustReturn2()
+        {
+            Assert.AreEqual(2, calc.Division());
+        }
+
+       
+    }
+}
---
+++ Calculator/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class CalculatorClass
+    {
+        public int a;
+        public int b;
+        public CalculatorClass(int A, int B)
+        {
+            a = A;
+            b = B;
+        }
+        public int Additional()
+        {
+            return a + b;
+        }
+
+        public int Subtraction()
+        {
+            return a - b;
+        }
+
+        public int Miltiplication()
+        {
+            return a * b;
+        }
+
+        public int Division()
+        {
+            return a / b;
+        }
+    }
+}
